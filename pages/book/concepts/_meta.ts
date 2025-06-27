0e145af87b7a3422f354035d416139c2ac8bf162
Dawoon Jung <tyranno2020@gmail.com>
2025-06-28 00:17:35 +0900

    docs: translate Concepts section to Korean

--- pages/book/concepts/_meta.ts
+++ pages/book/concepts/_meta.ts
@@ -3,6 +3,6 @@ export default {
   packages: '패키지',
   manifest: '패키지 매니페스트',
   address: '주소',
-  account: '계정',
-  transaction: '트랜잭션'
+  'what-is-an-account': '계정',
+  'what-is-a-transaction': '트랜잭션'
 }
